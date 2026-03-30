1987f9816cf929c5ab0d251da72b45207ed00bbb
Robson Schneider <robson.willian@universo.univates.br>
2026-03-30 14:19:54 -0300

    Ajustes de responsividade e interações JS

--- js/scripts.js
+++ js/scripts.js
@@ -7,3 +7,16 @@ function hideSidebar() {
   const sidebar = document.querySelector(".sidebar");
   sidebar.style.display = "none";
 }
+
+let titulo = document.querySelector(".maquina-escrever");
+typeWrite(titulo);
+
+function typeWrite(elemento) {
+  const textoArray = elemento.innerHTML.split("");
+  elemento.innerHTML = " ";
+  textoArray.forEach(function (letra, i) {
+    setTimeout(function () {
+      elemento.innerHTML += letra;
+    }, 200 * i);
+  });
+}
