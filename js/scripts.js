1bbc07d692595b7742ce7f06363aa702960bb3dc
Robson Schneider <robson.willian@universo.univates.br>
2026-04-08 12:28:52 -0300

    Adicionando Scrool Suave entre links

--- js/scripts.js
+++ js/scripts.js
@@ -27,12 +27,39 @@ function typeWrite(elemento) {
 
 function toggleArticle() {
   const artigos = document.querySelectorAll(".projetos-escondidos");
+  let bntVerMais = document.querySelector(".bnt-vermais");
 
   artigos.forEach((artigo) => {
     if (artigo.style.display === "block") {
       artigo.style.display = "none";
+      bntVerMais.innerHTML = "Ver Mais";
     } else {
       artigo.style.display = "block";
+      bntVerMais.innerHTML = "Ver Menos";
     }
   });
 }
+
+const menuLinks = document.querySelectorAll('#nav-header a[href^="#"]');
+
+function getDistanceFromTheTop(element) {
+  const id = element.getAttribute("href");
+  return document.querySelector(id).offsetTop;
+}
+
+function nativeScroll(distanceFromTheTop) {
+  window.scroll({
+    top: distanceFromTheTop,
+    behavior: "smooth",
+  });
+}
+
+function scrollToSection(event) {
+  event.preventDefault();
+  const distanceFromTheTop = getDistanceFromTheTop(event.target) - 115;
+  nativeScroll(distanceFromTheTop);
+}
+
+menuLinks.forEach((link) => {
+  link.addEventListener("click", scrollToSection);
+});
