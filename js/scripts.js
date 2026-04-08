36f54790554fa173b85cda898cf0b5ca059b18b5
Robson Schneider <robson.willian@universo.univates.br>
2026-04-08 10:17:13 -0300

    Adicionando bnt-vermais

--- js/scripts.js
+++ js/scripts.js
@@ -20,3 +20,19 @@ function typeWrite(elemento) {
     }, 220 * i);
   });
 }
+
+//const bntVerMais = document.getElementsByTagName(".bnt-vermais");
+
+//bntVerMais.addEventListener("click", toggleDiv);
+
+function toggleArticle() {
+  const artigos = document.querySelectorAll(".projetos-escondidos");
+
+  artigos.forEach((artigo) => {
+    if (artigo.style.display === "block") {
+      artigo.style.display = "none";
+    } else {
+      artigo.style.display = "block";
+    }
+  });
+}
